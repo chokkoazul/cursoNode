9724bfedb0aeab53fda3ceb8be6f0247ba030925
cristian osorio <osoriocri@gmail.com>
2018-07-01 22:45:45 -0400

    sesion 2

---
+++ 01-Hola-Mundo/app3.js
@@ -0,0 +1,15 @@
+console.log('inicio programa');
+
+setTimeout(function(){
+console.log('primer timeout');
+}, 3000);
+
+setTimeout(function(){
+    console.log('segundo timeout');
+    }, 0);
+
+setTimeout(function(){
+    console.log('tercero timeout');
+    }, 0);
+        
+console.log('fin programa');
