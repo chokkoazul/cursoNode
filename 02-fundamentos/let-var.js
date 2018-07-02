8280fd12ff13e5f14dace5a407079b99271461e8
cristian osorio <osoriocri@gmail.com>
2018-07-01 22:58:37 -0400

    let and var

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,18 @@
+let nombre = 'Wolverine';
+
+if (true){
+    let nombre = 'Magneto';
+}
+
+nombre = 'aaja';
+
+console.log(nombre);
+
+
+let i = 'Hola mundo';
+
+for(let i=0;i<5;i++){
+    console.log(`i=${i}`);
+}
+
+console.log(i);
